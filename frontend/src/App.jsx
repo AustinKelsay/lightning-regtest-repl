bb6b9fa65755072e8993078936f01a299f57f3e3
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2024-03-24 18:56:18 +0000

    Added in basic webln provider frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,104 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Channels from "./components/Channels";
+import AddPeer from "./components/AddPeer";
+import OpenChannel from "./components/OpenChannel";
 import "./App.css";
 
-export default function App() {
+function App() {
+  const [connectedNode, setConnectedNode] = useState({});
+  const [channels, setChannels] = useState([]);
+  const [onchainBalance, setOnchainBalance] = useState(0);
+  const [lightningBalance, setLightningBalance] = useState(0);
+
+  const loadAll = async function () {
+    await loadChannels();
+    await loadBalances();
+  };
+
   useEffect(() => {
-    axios
-      .get(
-        "https://caa84c5d-f25b-48a7-b657-893f7e430025-00-24lluiepd75yx.spock.replit.dev:8080/v1/getinfo",
-        {
-          headers: {
-            "Grpc-Metadata-macaroon":
-              "0201036c6e640269030a1045d5eaa0d5f646ce4d596d3dfcd070121201301a0c0a04696e666f1204726561641a100a08696e766f696365731204726561641a100a086f6666636861696e1204726561641a0f0a076f6e636861696e1204726561641a0e0a067369676e657212047265616400000620c3693fb057ce434939aa155abb635ed0feeff32b853e65a171427ac16d39b522",
-          },
-        },
-      )
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  return <main>React ⚛️ + Vite ⚡ + Replit</main>;
+    loadAll();
+  }, [connectedNode]);
+
+  const connect = async () => {
+    await window.webln.enable();
+
+    const info = await window.webln.getInfo();
+
+    console.log(info, "infooooo");
+
+    setConnectedNode(info?.node);
+    
+    // if (!info.methods || !info.methods.includes("walletbalance")) {
+    //   alert(
+    //     "Your connected WebLN provider is not supported. Please use Alby with a LND node",
+    //   );
+    //   document.location = "/";
+    //   return false;
+    // }
+    return true;
+  };
+
+  const loadChannels = async function () {
+    const nodeDetails = await window.webln.request("getnodeinfo", {
+      pub_key: connectedNode.pubkey,
+    });
+    console.log("nodeDetails", nodeDetails);
+    const result = await window.webln.request("listchannels");
+
+    const channels = result.channels;
+    console.log("channels", channels);
+    setChannels(
+      channels.sort((a, b) => {
+        return b.local_balance - a.local_balance;
+      }),
+    );
+  };
+
+  const loadBalances = async function () {
+    await window.webln.enable();
+    const channelbalance = await window.webln.request("channelbalance");
+    const walletbalance = await window.webln.request("walletbalance");
+    const onchain = walletbalance.total_balance;
+    const lightning = channelbalance.balance;
+    setLightningBalance(lightning);
+    setOnchainBalance(onchain);
+
+    console.log({ onchainBalance });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Node Dashboard</h1>
+        {connectedNode?.pubkey && <p>Connected to: {connectedNode.alias}</p>}
+      </header>
+      {/* connect button */}
+      {!connectedNode?.pubkey && (
+        <button onClick={connect}>Connect to your node</button>
+      )}
+      {/* connected */}
+      {connectedNode?.pubkey && <h2>Connected to {connectedNode?.pubkey}</h2>}
+      {/* balances */}
+      {connectedNode?.pubkey && (
+        <div className="balances">
+          <div className="balance">
+            <h3>Onchain balance</h3>
+            <p>{onchainBalance} sats</p>
+          </div>
+          <div className="balance">
+            <h3>Lightning balance</h3>
+            <p>{lightningBalance} sats</p>
+          </div>
+        </div>
+      )}
+      {/* add peer */}
+      {connectedNode?.pubkey && <AddPeer />}
+      {/* open channel */}
+      {connectedNode?.pubkey && <OpenChannel />}
+      {/* channels */}
+      <Channels channels={channels} />
+    </div>
+  );
 }
+
+export default App;
